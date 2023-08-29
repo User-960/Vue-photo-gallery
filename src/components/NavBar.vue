f9202e6cf17123fb66f7733bc651a531609f626f
Ivan <ivanvolodin960@gmail.com>
2023-08-29 20:10:21 +0300

    feat: create component NavBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-container fluid>
+    <v-app-bar
+      color="orange"
+      dark
+    >
+      <v-spacer></v-spacer>
+      <v-btn outlined class="ml-3">Main</v-btn>
+      <v-btn outlined class="ml-3">Photos</v-btn>
+    </v-app-bar>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+    name: 'NavBar',
+    components: {},
+  })
+</script>
+
+<style lang="">
+  
+</style>
